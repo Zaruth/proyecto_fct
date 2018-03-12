bc51d1b93512f95894a05ef0e7621a6befbbd2ed
Zaruth <elmensajerodelalba@gmail.com>
2018-03-12 14:42:21 +0100

    Arreglo SOAP.

--- src/FctBundle/Controller/DefaultController.php
+++ src/FctBundle/Controller/DefaultController.php
@@ -3,16 +3,36 @@
 namespace FctBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller {
 
+    private $session;
+
+    public function __construct() {
+        $this->session = new Session();
+    }
+    
     public function indexAction() {
         
-        $result = $this->geolocalizacion();
+        
+        $this->iniciaSession();
         
         return $this->render('FctBundle:Default:index.html.twig',array(
-                "result" => $result
+                "pais" => $this->session->get('pais'),
+                "latitud" => $this->session->get('latitud'),
+                "longitud" => $this->session->get('longitud')
             ));
+    }
+    
+    public function iniciaSession() {
+        if(!$this->get('session')->has('pais') || !$this->get('session')->has('longitud') || !$this->get('session')->has('latitud')){
+            $result = $this->geolocalizacion();
+            $this->get('session')->set('pais', $result["ResolveIPResult"]["Country"]);
+            $this->get('session')->set('longitud', $result["ResolveIPResult"]["Longitude"]);
+            $this->get('session')->set('latitud', $result["ResolveIPResult"]["Latitude"]);
+        }
     }
     
     public function geolocalizacion(){
